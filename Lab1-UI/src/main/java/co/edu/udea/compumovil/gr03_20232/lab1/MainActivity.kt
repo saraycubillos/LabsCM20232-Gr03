04ed2cb516f5183a484c1fcba01723b1cb341292
Victor Manuel Osorio Garcia <victormanuelosoriog@gmail.com>
2023-09-07 20:35:39 -0500

    Initial commit

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20232/lab1/MainActivity.kt
@@ -0,0 +1,43 @@
+package co.edu.udea.compumovil.gr03_20232.lab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.udea.compumovil.gr03_20232.lab1.ui.theme.Labs20232Gr03Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Labs20232Gr03Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Labs20232Gr03Theme {
+        Greeting("Android")
+    }
+}
